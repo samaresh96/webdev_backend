e3a699f73a4257e7919a29d93d2fba3ac07ff152
Samaresh Shil <108946101+samaresh96@users.noreply.github.com>
2025-04-25 13:41:35 +0530

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: 'db', // important for Docker
+  host: process.env.DB_HOST,  // important for Docker
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432
