bf0dd0bccfa45da9211370c431baff2ea5de6403
Samaresh Shil <108946101+samaresh96@users.noreply.github.com>
2025-04-25 13:49:15 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS form_data (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100),
   age INT,
-  desire VARCHAR(100)
-  hobby TEXT,
+  desire VARCHAR(100),
+  hobby VARCHAR(100),
   email VARCHAR(100)
 );
