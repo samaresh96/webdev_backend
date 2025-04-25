a6dd7bb90d75b5e7573fe3f34867b4e343a10646
Samaresh Shil <108946101+samaresh96@users.noreply.github.com>
2025-04-25 15:04:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import formRoutes from './routes/formRoutes.js';
 const app = express();
 
 app.use(cors());
+
+app.use(cors({ origin: 'http://65.1.100.7/' })); // replace with your frontend URL
+
 app.use(express.json());
 
 app.use('/api', formRoutes);
